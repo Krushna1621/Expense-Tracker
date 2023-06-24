cc9f98b8ebf6a32e1ef05005cfd3de521f79a9d3
Krushna1621 <shindekrushna825@gmail.com>
2023-06-25 02:17:27 +0530

    expense-tracker task 6

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import classes from './Header.module.css'
-
+import { NavLink, useHistory } from 'react-router-dom'
+import AuthContext from '../../store/auth-context'
 const Header = () => {
+  const authCtx = useContext(AuthContext)
+  const history = useHistory()
+
+  const logoutHandler = (e) => {
+    e.preventDefault()
+
+    authCtx.logout()
+    history.replace('/login')
+  }
+
   return (
     <header className={classes.header}>
-      {/* { <h4>My Weblinky</h4> } */}
       <ul className={classes.navList}>
-        <li>
-          HOME
-        </li>
-        <li>
-          PRODUCTS
-        </li>
-        <li>
-          ABOUT Us
-        </li>
+        <li>HOME</li>
+        <li>PRODUCTS</li>
+        <li>ABOUT Us</li>
+        {authCtx.isLoggedIn && <button onClick={logoutHandler}>Logout</button>}
       </ul>
     </header>
   )
